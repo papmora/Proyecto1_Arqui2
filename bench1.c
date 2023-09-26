9508c498dcb8975853d42ab3311ca3b9ef6e625a
Zeik <royzeph@gmail.com>
2023-09-26 04:06:55 -0600

    System Info function

--- bench1.c
+++ bench1.c
@@ -4,6 +4,8 @@
 #include <stdint.h>
 #include <inttypes.h>
 #include <sys/time.h>
+#include <sys/sysinfo.h>
+#include <unistd.h>
 
 #define ARRAY_SIZE 1000000
 #define NUM_THREADS 4
@@ -35,7 +37,87 @@ void *writeToSharedArray(void *threadId) {
     pthread_exit(NULL);
 }
 
+int systemInformation() {
+    // Guardar info en txt
+    system("lscpu > lscpu_output.txt");
+
+    // Obtencion del size de una Linea de cache de L1 - Datos
+    long l1_cache_line_size = sysconf(_SC_LEVEL1_DCACHE_LINESIZE);
+    // Obtencion del size de una Instancia de cache de L1 - Datos
+    long l1_cache_size = sysconf(_SC_LEVEL1_DCACHE_SIZE);
+
+    // Validacion de L1 - Datos
+    if (l1_cache_line_size == -1 || l1_cache_size == -1) {
+        perror("Error al obtener el tamaños de cache");
+        return 1;
+    }
+
+    // Obtencion del size de una Linea de cache de L1 - Datos
+    long l2_cache_line_size = sysconf(_SC_LEVEL2_CACHE_LINESIZE);
+    // Obtencion del size de una Instancia de cache de L1 - Datos
+    long l2_cache_size = sysconf(_SC_LEVEL2_CACHE_SIZE);
+
+    // Validacion de L1 - Datos
+    if (l2_cache_line_size == -1 || l2_cache_size == -1) {
+        perror("Error al obtener el tamaños de cache");
+        return 1;
+    }
+
+    // Obtencion del size de una Linea de cache de L1 - Datos
+    long l3_cache_line_size = sysconf(_SC_LEVEL3_CACHE_LINESIZE);
+    // Obtencion del size de una Instancia de cache de L1 - Datos
+    long l3_cache_size = sysconf(_SC_LEVEL3_CACHE_SIZE);
+
+    // Validacion de L1 - Datos
+    if (l3_cache_line_size == -1 || l3_cache_size == -1) {
+        perror("Error al obtener el tamaños de cache");
+        return 1;
+    }
+
+    // Pagina de RAM
+    long page_size = sysconf(_SC_PAGESIZE);
+    // Cantidad total de RAM en bytes:
+    long num_phys_pages = sysconf(_SC_PHYS_PAGES);
+    // Conversion:
+    long long total_memory = (long long)page_size * num_phys_pages;
+    // RAM en Gigas:
+    double total_memory_gb = (double)total_memory / (1024 * 1024 * 1024);
+
+    // Obtencion precisa de CPU y Threads
+    long num_cores = sysconf(_SC_NPROCESSORS_ONLN);
+    long num_threads = sysconf(_SC_NPROCESSORS_CONF);
+
+    printf("-----Especificaciones del sistema-----\n");
+    printf("Número de núcleos(Number of Cores): %ld\n", num_cores);
+    printf("Número de hilos(Number of Threads): %ld\n", num_threads);
+    system("lscpu | grep 'Thread(s) per core'");
+    system("lscpu | grep 'Core(s) per socket'");
+    printf("Cantidad total de memoria RAM: %.2f GB\n", total_memory_gb);
+    // Cache L1:
+    printf("Cache L1:\n");
+    printf("Tamaño de línea: %ld bytes\n", l1_cache_line_size);
+    printf("Tamaño de Instancia: %ld bytes\n", l1_cache_size);
+    system("lscpu | grep 'L1d cache'");
+    // Cache L2:
+    printf("Cache L2:\n");
+    printf("Tamaño de línea: %ld bytes\n", l2_cache_line_size);
+    printf("Tamaño de Instancia: %ld bytes\n", l2_cache_size);
+    system("lscpu | grep 'L2 cache'");
+    // Cache L3:
+    printf("Cache L3:\n");
+    printf("Tamaño de línea: %ld bytes\n", l3_cache_line_size);
+    printf("Tamaño de Instancia: %ld bytes\n", l3_cache_size);
+    system("lscpu | grep 'L3 cache'");
+
+    printf("Procesador:\n");
+    system("lscpu | grep 'Model name'");
+}
+
 int main() {
+    
+    // UwU
+    systemInformation();
+
     pthread_t threads[NUM_THREADS];
     struct timeval start, end;
     uint64_t elapsed;
